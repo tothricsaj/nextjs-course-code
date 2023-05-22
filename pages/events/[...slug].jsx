ffc62625f96e70b31862d3a8368deb53f0da653c
tothricsaj <tothricsaj@gmail.com>
2023-05-22 18:29:37 +0200

    feat: basic setup of routes

---
+++ pages/events/[...slug].jsx
@@ -0,0 +1,9 @@
+function FilteredEventsPage() {
+  return (
+    <div>
+      <h2>Filtered Events</h2>
+    </div>
+  )
+}
+
+export default FilteredEventsPage
---
+++ pages/events/[eventId].jsx
@@ -0,0 +1,9 @@
+function EventDetailPage() {
+  return (
+    <div>
+      <h2>Event detail</h2>
+    </div>
+  )
+}
+
+export default EventDetailPage;
