7791d3639de31198477184da32abbd33bf2141c9
tothricsaj <tothricsaj@gmail.com>
2023-05-22 19:26:17 +0200

    feat: styling events

--- components/events/event-item.jsx
+++ components/events/event-item.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import classes from './event-item.module.css';
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -11,19 +12,19 @@ function EventItem(props) {
   const expolreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={`/${image}`} alt={title} />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{humanReadableData}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address>{formatedAddress}</address>
           </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={expolreLink}>Expolore Events</Link>
         </div>
       </div>
