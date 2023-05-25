0eb9df01345e9fae3a74287a5a14ccb158c7ea1e
tothricsaj <tothricsaj@gmail.com>
2023-05-25 15:42:00 +0200

    feat: added general layout wrapper

---
+++ components/layout/layout.jsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+import MainHeader from "./main-header";
+
+function Layout(props) {
+
+  return (
+    <Fragment>
+      <MainHeader />
+      {props.children}
+    </Fragment>
+  )
+}
+
+export default Layout;
