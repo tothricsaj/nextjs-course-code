0670177211d08bb3a7d6c8dabfe101b605f4fbe4
tothricsaj <tothricsaj@gmail.com>
2023-05-27 16:26:34 +0200

    feat: added event search component
    It does nothing

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,8 +1,14 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from '../../components/events/event-list';
+import EventSearch from "../../components/events/event-search";
+
 function AllEventsPage() {
+  const events = getAllEvents();
   return (
-    <div>
-      <h1>All Events</h1>
-    </div>
+    <>
+      <EventSearch />
+      <EventList items={events} />
+    </>
   )
 }
 
