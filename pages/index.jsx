7184212516bfa0e58ecf437881846fe465bca2ac
tothricsaj <tothricsaj@gmail.com>
2023-05-28 18:50:49 +0200

    feat: running servers side code and using filesystem

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,9 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 function Home(props) {
   const { products } = props;
+
   return (
     <ul>
       {
@@ -12,9 +16,13 @@ function Home(props) {
 }
 
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data/dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [{id: 'p1', title: 'Product 1'}],
+      products: data.products,
     }
   };
 }
