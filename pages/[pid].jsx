b76e9eda275a8e7a4c34cbb263142597050d8653
tothricsaj <tothricsaj@gmail.com>
2023-05-30 10:00:56 +0200

    feat: dynamic patsh

--- pages/[pid].jsx
+++ pages/[pid].jsx
@@ -11,13 +11,21 @@ function ProductDetailPage(props) {
   );
 }
 
-export async function getStaticProps(context) {
-  const { params } = context;
+async function getData() {
   const filePath = path.join(process.cwd(), 'data/dummy-backend.json');
 
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  return data;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+
   const productId = params.pid
+
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
 
@@ -29,10 +37,13 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+  const pathsWithParams = ids.map((id) => ({params: {pid: id}}));
+
   return {
-    paths: [
-      { params: { pid: 'p1' } }
-    ],
+    paths: pathsWithParams,
     fallback: 'blocking'
   }
 }
