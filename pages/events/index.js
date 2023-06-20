4ff0b5f10fbf6af2f48796ddec3cc1bcbe137120
tothricsaj <tothricsaj@gmail.com>
2023-06-20 09:52:05 +0200

    feat: added loading

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,18 +1,26 @@
 import { Fragment } from 'react';
 import { useRouter } from 'next/router';
 
-import { getAllEvents } from '../../dummy-data';
+import { getAllEvents } from '../../helpers/api-util';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 
-function AllEventsPage() {
+function AllEventsPage(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  const events = props.events;
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
     
     router.push(fullPath);
+  }
+
+  if(!events) {
+    return (
+      <div className='center'>
+        <p>Loading...</p>
+      </div>
+    );
   }
 
   return (
@@ -21,6 +29,17 @@ function AllEventsPage() {
       <EventList items={events} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events
+    },
+    revalidate: 60
+  }
 }
 
 export default AllEventsPage;
